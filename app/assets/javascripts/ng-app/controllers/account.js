407232efb1f1b54561161e80a2b3eb36d6445bc8
Jason Shultz <jasshultz@gmail.com>
2015-04-07 17:00:03 -0600

    user authentication stuff

--- app/assets/javascripts/ng-app/controllers/account.js
+++ app/assets/javascripts/ng-app/controllers/account.js
@@ -1,9 +1,19 @@
 angular.module('AngularRails')
-    .controller('AcctCtrl', ["currentAuth", "Auth", "$scope","$location","$timeout", 
-        function(currentAuth, Auth, $scope, $location, $timeout)  {
+    .controller('AcctCtrl', ["currentAuth", "Auth", "$scope","$location","$timeout","UserData", 
+        function(currentAuth, Auth, $scope, $location, $timeout, UserData)  {
 
     $scope.auth = Auth;
 
     var ref = new Firebase("https://rails-angular-fireba.firebaseio.com");
+
+    // any time auth status updates, add the user data to scope
+      $scope.auth.$onAuth(function(authData) {
+        $scope.authData = authData;
+        if (authData) {
+        	$scope.displayName = UserData.getName(authData)
+        }
+
+      });
+
 
 }]);
