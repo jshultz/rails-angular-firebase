5015d305b500b12175d88e177b0505bd0e982ceb
Jason Shultz <jasshultz@gmail.com>
2015-04-13 08:52:31 -0600

    ading step1 for account creation

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -26,6 +26,20 @@ angular
               return Auth.$requireAuth();
             }]
           }
+        }).when("/account/step1", {
+          // the rest is the same for ui-router and ngRoute...
+          name: "account",
+          controller: "AcctCtrl",
+          templateUrl: "accounts/create/step1.html",
+          resolve: {
+            // controller will not be loaded until $requireAuth resolves
+            // Auth refers to our $firebaseAuth wrapper in the example above
+            "currentAuth": ["Auth", function(Auth) {
+              // $requireAuth returns a promise so the resolve waits for it to complete
+              // If the promise is rejected, it will throw a $stateChangeError (see above)
+              return Auth.$requireAuth();
+            }]
+          }
         }).when("/about", {
           // the rest is the same for ui-router and ngRoute...
           name: "about",
