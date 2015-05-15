fc4f3586de011c35276b2af0b5c8a9a3901d1d5f
Jason Shultz <jasshultz@gmail.com>
2015-05-15 07:01:24 -0600

    still working on user levels

--- app/assets/javascripts/ng-app/factories/userData.js
+++ app/assets/javascripts/ng-app/factories/userData.js
@@ -84,23 +84,29 @@
 			if (authData) {
 
                 ref.child('users').once('value', function(snapshot) {
-                   if (snapshot.val() === null) { // there are no users
-                    ref.child("users").child(authData.uid).set({
+
+                    var child = snapshot.hasChildren()
+
+                    if (child == false) {
+
+                        ref.child("users").child(authData.uid).set({
                         provider: authData.provider,
                         full_name: factory.getName(authData),
                         user_level: 1
                     });
-                    console.log('no users')
-                       /* There is no user 'Fred'! */
-                   } else { // there are users
-                    ref.child("users").child(authData.uid).set({
+
+                        console.log('no users')
+
+                    } else {
+                        ref.child("users").child(authData.uid).set({
                         provider: authData.provider,
                         full_name: factory.getName(authData),
                         user_level: 10
                     });
+
                         console.log('there are users')
-                       /* User 'Fred' exists. */
-                   }
+                    }
+
                 });
 			  // save the user's profile into Firebase so we can list users,
 			  // use them in Security and Firebase Rules, and show profiles
